4540f127bdc494381537a9d76273232689c2982f
ShawnXxy <shawnxxy@hotmail.com>
2017-07-24 21:20:23 +0800

    to be continued

--- src/play/Game.java
+++ src/play/Game.java
@@ -7,7 +7,12 @@ public class Game implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-
+		GameCore.setGameSize(338, 600);
+		GameCore.loadBgView("background.png");
+		while (true) {
+			GameSprite hero = new GameSprite("HeroPlane");
+			GameCore.pause(10);
+		}
 	}
 
 	public static void main(String[] args) {
